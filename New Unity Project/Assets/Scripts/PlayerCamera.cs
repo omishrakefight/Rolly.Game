2ea2c14b7f435abad952487459b7a88f980f89e2
omishrakefight <omishrakefight@live.com>
2018-01-13 19:52:33 -0500

    Working on triggers
    Got Camera to swap on trigger, need to inverse moving after trigger, and set up return camera swaps

--- New Unity Project/Assets/Scripts/PlayerCamera.cs
+++ New Unity Project/Assets/Scripts/PlayerCamera.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour {
 
+    public Camera SecondCamera;
+    public Camera MainCamera;
     public GameObject player;
 
     private Vector3 offset;
@@ -11,12 +13,38 @@ public class PlayerCamera : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         offset = transform.position - player.transform.position;
-	}
+        ShowMainCamera();
+     
+    }
 	
 	// Update is called once per frame
 	void LateUpdate () {
         transform.position = player.transform.position + offset;
 		
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            ShowSecondCamera();
+    }
+
+    public void ShowSecondCamera()
+    {
+        MainCamera.enabled = false;
+        SecondCamera.enabled = true;
+    }
+
+    public void ShowMainCamera()
+    {
+        SecondCamera.enabled = false;
+        MainCamera.enabled = true;
+    }
+
+
+
+
+
 }
-camera
+
+
