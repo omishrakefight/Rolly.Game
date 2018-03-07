2b110ff23c4f32a32ee680e4c30734148c9cf427
omishrakefight <omishrakefight@live.com>
2018-03-06 19:20:47 -0500

    3rd Level created
    Spin blocks, first safe zone.

---
+++ Rolly Ball Game/Assets/Scripts/Objects/X_flip_blocks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class X_flip_blocks : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(30, 0, 0) * Time.deltaTime);
+	}
+}
