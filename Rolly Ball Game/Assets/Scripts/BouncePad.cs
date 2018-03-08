0551d9cf9bbaf98032ef779720a2cdcbdd1f46b6
Kyle_Laptopbro <omishrakefight@live.com>
2018-03-08 14:29:10 -0500

    New script
    Tried to create a bounce pad during class, can't concentrate / experiment enough.  Its started though

---
+++ Rolly Ball Game/Assets/Scripts/BouncePad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //other.gameObject.rigidbody
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
